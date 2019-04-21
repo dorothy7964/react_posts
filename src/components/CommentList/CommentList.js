07805c91b56becae4338bb404f3ca3cae645e757
SEC\user <dorothy_007@naver.com>
2019-04-21 23:34:44 +0900

    전달받은 props 렌더링
    Post, CommentList, Comment 컴포넌트 수정

--- src/components/CommentList/CommentList.js
+++ src/components/CommentList/CommentList.js
@@ -3,14 +3,23 @@ import { Comment } from  '../';
 
 import './CommentList.css';
 
-const CommentList = () => {
+const CommentList = ({comments}) => {
+  // map data to components
+  const commentList = comments.map(
+    (comment, index)=>(
+      <Comment
+        name={comment.name}
+        body={comment.body}
+        key={index}
+      />
+    )
+  );
+
   return (
     <ul className="CommentList">
-      <Comment />
-      <Comment />
-      <Comment />
+      {commentList}
     </ul>
   );
-}
+};
 
 export default CommentList;
